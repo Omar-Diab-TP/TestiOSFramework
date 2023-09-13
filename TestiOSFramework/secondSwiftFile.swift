80d791ece22d1ca59c2b703da4c131010d407904
Omar-Diab-TP <omar.diab@tamatemplus.com>
2023-09-13 18:10:20 +0300

    initial commit
    project created with a sample class.

---
+++ TestiOSFramework/secondSwiftFile.swift
@@ -0,0 +1,20 @@
+//
+//  secondSwiftFile.swift
+//  
+//
+//  Created by Omar Diab on 13/09/2023.
+//
+
+import Foundation
+
+public class secondSwiftFile {
+    private var name: String
+    
+    public init(name:String) {
+        self.name = name
+    }
+    
+    public func getName() -> String {
+        return name
+    }
+}
---
+++ TestiOSFramework/testFramework.swift
@@ -0,0 +1,16 @@
+//
+//  testFramework.swift
+//  
+//
+//  Created by Omar Diab on 12/09/2023.
+//
+
+import Foundation
+ 
+public class testFramework {
+    
+    //test
+    public func printHello () {
+        print("Hello world")
+    }
+}
