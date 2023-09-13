ef49d70df4cf14e0a1aac61af88e0bba2caf3a3f
Omar-Diab-TP <omar.diab@tamatemplus.com>
2023-09-13 17:42:00 +0300

    Initial Commit

---
+++ TestiOSFramework/TestiOSFramework.h
@@ -0,0 +1,18 @@
+//
+//  TestiOSFramework.h
+//  TestiOSFramework
+//
+//  Created by Omar Diab on 13/09/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestiOSFramework.
+FOUNDATION_EXPORT double TestiOSFrameworkVersionNumber;
+
+//! Project version string for TestiOSFramework.
+FOUNDATION_EXPORT const unsigned char TestiOSFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestiOSFramework/PublicHeader.h>
+
+
